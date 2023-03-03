bfcf5ee859e4546a16f6b6bc41eeeb375319ec84
Dave <Daverevell491@gmail.com>
2023-03-03 15:56:17 +0000

    WIP handling data changes with hardcoded values

--- src/components/BaseStat.jsx
+++ src/components/BaseStat.jsx
@@ -1,9 +1,34 @@
 export default function BaseStat({ stat }) {
+	let statValue = "";
+
+	const handleChange = (e) => {
+		const name = e.target.name;
+		let value = e.target.value;
+		console.log(name, value);
+		if (name === "strength") {
+			value = Number(value);
+		}
+	};
+
 	return (
 		<li>
 			<label htmlFor={stat}>{stat}</label>
-			<input type="text" id={stat} className="stat" />
-			<input type="text" id={`${stat}-mod`} className="stat-mod" />
+			<input
+				type="text"
+				id={stat}
+				className="stat"
+				name={stat}
+				value={statValue}
+				onChange={handleChange}
+			/>
+			<input
+				type="text"
+				id={`${stat}-mod`}
+				className="stat-mod"
+				// TODO: this only works for - stat mods for example 8/9 is -1, but 11 is +1
+				value={((25 - 10) / 2).toFixed()}
+				onChange={handleChange}
+			/>
 		</li>
 	);
 }
