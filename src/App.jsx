e83fefd0cabdb55618507a3e598660778d8ea649
Dave <Daverevell491@gmail.com>
2023-03-06 16:20:07 +0000

    ico added, very important

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ function App() {
 	return (
 		<>
 			<CharacterSheet />
+
+			{/*
+        TODO: Add this to a footer
+        <a href="https://www.flaticon.com/free-icons/d20" title="d20 icons">
+				D20 icon by Freepik
+			  </a> 
+      */}
 		</>
 	);
 }
--- src/components/BaseStat.jsx
+++ src/components/BaseStat.jsx
@@ -26,7 +26,7 @@ export default function BaseStat({ stat }) {
 				id={`${stat}-mod`}
 				className="stat-mod"
 				// TODO: this only works for - stat mods for example 8/9 is -1, but 11 is +1
-				value={((25 - 10) / 2).toFixed()}
+				value={((20 - 10) / 2).toFixed()}
 				onChange={handleChange}
 			/>
 		</li>
