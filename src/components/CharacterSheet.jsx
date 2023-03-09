d112f43108e6398d055d56990d17c6377cd7be31
Dave <Daverevell491@gmail.com>
2023-03-09 14:00:23 +0000

    basic submit form

--- src/components/CharacterSheet.jsx
+++ src/components/CharacterSheet.jsx
@@ -3,11 +3,21 @@ import CharacterSheetHeader from "./CharacterSheetHeader";
 import CSPageOneMain from "./CSPageOneMain";
 import Footer from "./Footer";
 import Header from "./Header";
+import { Button } from "@mui/material";
 
 const apiUrl = "http://localhost:4000";
 
 export default function CharacterSheet() {
 	const [characterSheet, setCharacterSheet] = useState([]);
+
+	const buttonStyling = {
+		display: "flex",
+		backgroundColor: "#1976d2",
+		color: "white",
+		justifyContent: "space-between",
+		alignContent: "center",
+		padding: "1em",
+	};
 
 	useEffect(() => {
 		fetchCS();
@@ -24,10 +34,27 @@ export default function CharacterSheet() {
 		return <p>Loading</p>;
 	}
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			// TODO: Hard coded for now
+			const res = await fetch(`${apiUrl}/characterSheet/1`);
+			const data = await res.json();
+			setCharacterSheet([...characterSheet, data.data[0]]);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<>
 			<Header />
-			<form className="sheet">
+
+			<form className="sheet" onSubmit={handleSubmit}>
+				<Button className="button" type="submit" sx={buttonStyling}>
+					Save
+				</Button>
 				<CharacterSheetHeader
 					characterSheet={characterSheet}
 					setCharacterSheet={setCharacterSheet}
