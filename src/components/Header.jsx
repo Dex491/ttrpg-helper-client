2e7cf9d26dd5de9d3c8f273b7a3a0531804e5ea5
Dave <Daverevell491@gmail.com>
2023-03-09 11:15:55 +0000

    header complete enough for presentation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
 	return (
 		<header>
 			<AppBar>
-				<Toolbar>
+				<Toolbar className="toolBar">
 					<div className="headerLogo">
 						<img src="././public/d20-header.png" alt="TTRPG Helper logo" />
 						<Typography variant="h6" component="h1">
@@ -20,8 +20,36 @@ export default function Header() {
 							alignContent: "center",
 							padding: "1em",
 						}}
+						href="/"
+					>
+						Home
+					</Button>
+					<Button
+						className="push"
+						sx={{
+							display: "flex",
+							backgroundColor: "grey",
+							color: "white",
+							justifyContent: "space-between",
+							alignContent: "center",
+							padding: "1em",
+						}}
+						href="/characterSheet"
 					>
 						Character Sheets
+					</Button>
+					<Button
+						sx={{
+							display: "flex",
+							backgroundColor: "grey",
+							color: "white",
+							justifyContent: "space-between",
+							alignContent: "center",
+							padding: "1em",
+						}}
+						href="/logOut"
+					>
+						Log out
 					</Button>
 				</Toolbar>
 			</AppBar>
