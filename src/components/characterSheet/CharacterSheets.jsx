eb8fe9fe9c178f0686c3de4704016777c4a623a2
Dave <Daverevell491@gmail.com>
2023-03-15 11:01:22 +0000

    fetching all CS data ready to create cards

--- src/components/characterSheet/CharacterSheets.jsx
+++ src/components/characterSheet/CharacterSheets.jsx
@@ -1,8 +1,22 @@
+import { useState, useEffect } from "react";
 import Footer from "../Footer";
 import Header from "../Header";
 
+const apiUrl = "http://localhost:4000";
+
 export default function CharacterSheets() {
-	const [characterSheets, setCharacterSheets] = useState(null);
+	const [characterSheets, setCharacterSheets] = useState([]);
+
+	useEffect(() => {
+		fetchAllCS();
+	}, []);
+
+	const fetchAllCS = async () => {
+		const res = await fetch(`${apiUrl}/characterSheet`);
+		const data = await res.json();
+		setCharacterSheets(data);
+		console.log(characterSheets);
+	};
 	return (
 		<>
 			<Header />
